da5d568ece0296102b88ab84aafe2af532dea6d9
chuanchuan <62045791+89461561511656@users.noreply.github.com>
2021-07-07 19:35:08 +0800

    吃货

---
+++ eat/three_punch_combo.js
@@ -0,0 +1,12 @@
+        $i = 0;
+        $('#start').click(function(){
+            $i++;
+            if($i >=6 ){
+                $('#start').hide();
+                $('#stop').show();
+            }
+        })
+        $('#stop').click(function(){
+            alert('这么挑剔？我看你想吃空气！')
+            $(this).hide();
+        })
